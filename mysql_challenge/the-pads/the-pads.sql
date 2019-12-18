2426f3c5c39883df87e64e7f6bb2380b622ecc39
aniekan <foo@example.com>
2019-12-18 16:55:42 +0100

    the-pads-solved

--- mysql_challenge/the-pads/the-pads.sql
+++ mysql_challenge/the-pads/the-pads.sql
@@ -1,3 +1,4 @@
+
 SELECT CONCAT(Name, '(', SUBSTR(Occupation,1,1),')') FROM OCCUPATIONS ORDER BY Name;
 SELECT CONCAT('There are a total of ', COUNT(Occupation), ' ', LOWER(Occupation), 's.')
 FROM OCCUPATIONS 
